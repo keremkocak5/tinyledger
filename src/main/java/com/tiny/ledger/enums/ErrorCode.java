a62030fccad67889b669cc6830bcc688a5f7cd92
keremkocak5 <72099354+keremkocak5@users.noreply.github.com>
2025-04-07 22:01:00 +0100

    Error message edited

--- src/main/java/com/tiny/ledger/enums/ErrorCode.java
+++ src/main/java/com/tiny/ledger/enums/ErrorCode.java
@@ -8,7 +8,7 @@ public enum ErrorCode {
 
     BALANCE_NEGATIVE("I0001", "Balance cannot be negative", HttpStatus.NOT_ACCEPTABLE),
     ACCOUNT_NOT_FOUND("I0002", "Account not found", HttpStatus.NOT_FOUND),
-    DUPLICATE_UUID("I0003", "Are you lucky or unlucky? UUID already generated!", HttpStatus.INTERNAL_SERVER_ERROR),
+    DUPLICATE_UUID("I0003", "UUID already generated, please try again", HttpStatus.INTERNAL_SERVER_ERROR),
     INTERNAL_SERVER_ERROR("I0000", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String errorCode;
